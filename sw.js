66744947bd9e0ad4268f3a57fddf9009f3b16daa
Nikita <nikita.k@bigdropinc.com>
2017-02-15 15:15:08 +0200

    cachesPolyfill fix

--- sw.js
+++ sw.js
@@ -23,7 +23,7 @@ self.addEventListener('install', function(event) {
 
     console.info('installed');
     event.waitUntil(
-        caches.open(CACHE_NAME)
+        cachesPolyfill.open(CACHE_NAME)
             .then(function(cache) {
                 return cache.addAll(urlsToCache);
             })
@@ -32,7 +32,7 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
+        cachesPolyfill.match(event.request).then(function(response) {
             return response || fetch(event.request);
         })
     );
